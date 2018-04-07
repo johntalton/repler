a6d5da0762ced2815a4b4f01a96eef0503f0d822
johntalton <johntalton@gmail.com>
2018-04-07 04:19:33 +0000

    removeHistoryDuplicates

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ resolve();  } }
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  completer: completer
+  completer: completer,
+
+  removeHistoryDuplicates: true
 });
 
 
